3ebd13d2c9e22873f6558f5d4050b74f237c242d
Jayanth Krishnamurthy <jayanth.krishnamurthy@ibm.com>
2025-02-26 09:33:19 +0530

    updates to primitives.h and other files - v1

--- src/coreclr/debug/ee/ppc64le/primitives.cpp
+++ src/coreclr/debug/ee/ppc64le/primitives.cpp
@@ -1,9 +1,34 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-//
-
 #include "stdafx.h"
 #include "threads.h"
 #include "../../shared/ppc64le/primitives.cpp"
 
+void CopyREGDISPLAY(REGDISPLAY* pDst, REGDISPLAY* pSrc)
+{
+    DT_CONTEXT tmp;
+    CopyRegDisplay(pSrc, pDst, &tmp);
+}
+
+void SetSSFlag(CONTEXT* pContext, Thread* pThread)
+{
+    _ASSERTE(pThread != NULL);
+
+    pThread->EnableSingleStep();
+}
+
+void UnsetSSFlag(CONTEXT* pContext, Thread* pThread)
+{
+    _ASSERTE(pThread != NULL);
+
+    pThread->DisableSingleStep();
+}
+
+// Check if single stepping is enabled.
+bool IsSSFlagEnabled(CONTEXT* pContext, Thread* pThread)
+{
+    _ASSERTE(pThread != NULL);
+
+    return pThread->IsSingleStepEnabled();
+}
