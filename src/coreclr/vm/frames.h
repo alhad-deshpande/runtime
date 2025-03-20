e45e96cc6f8ffd3cdf747fd8e82bae63d20917f9
Jayanth Krishnamurthy <jayanth.krishnamurthy@ibm.com>
2025-03-20 15:20:38 +0530

    ppc64le/primitives.cpp,vm/callcounting.h,vm/frames.h, vm/gccover.h

--- src/coreclr/vm/frames.h
+++ src/coreclr/vm/frames.h
@@ -855,6 +855,9 @@ public:
 #elif defined(TARGET_RISCV64)
             Object** firstIntReg = (Object**)&this->GetContext()->Gp;
             Object** lastIntReg  = (Object**)&this->GetContext()->T6;
+elif defined(TARGET_POWERPC64)
+            Object** firstIntReg = (Object**)&this->GetContext()->GPR[0];  
+            Object** lastIntReg  = (Object**)&this->GetContext()->GPR[31]; 
 #else
             _ASSERTE(!"nyi for platform");
 #endif
