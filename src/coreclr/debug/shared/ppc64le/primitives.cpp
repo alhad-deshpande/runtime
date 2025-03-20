58b3f918a5afefdce9fb6b15ccec20409b5147b0
Jayanth Krishnamurthy <jayanth.krishnamurthy@ibm.com>
2025-03-20 15:39:28 +0530

    update

--- src/coreclr/debug/shared/ppc64le/primitives.cpp
+++ src/coreclr/debug/shared/ppc64le/primitives.cpp
@@ -1,5 +1,16 @@
 
-
+// Licensed to the .NET Foundation under one or more agreements.
+ // The .NET Foundation licenses this file to you under the MIT license.
+ //*****************************************************************************
+ // File: primitives.cpp
+ //
+ 
+ //
+ // Platform-specific debugger primitives
+ //
+ //*****************************************************************************
+ 
+ #include "primitives.h"
 
 //
  // CopyThreadContext() does an intelligent copy from pSrc to pDst,
