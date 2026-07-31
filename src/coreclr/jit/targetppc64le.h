227e02cb063de39105ff0be5f07eb8b29387cb94
Alhad Deshpande <Alhad.Deshpande1@ibm.com>
2026-07-31 07:10:46 -0400

    Enabled FEATURE_EH

--- src/coreclr/jit/targetppc64le.h
+++ src/coreclr/jit/targetppc64le.h
@@ -39,7 +39,7 @@
   #define USER_ARGS_COME_LAST      1
   #define EMIT_TRACK_STACK_DEPTH   1       // This is something of a workaround.  For both ARM and AMD64, the frame size is fixed, so we don't really
   #define TARGET_POINTER_SIZE 8 // equal to sizeof(void*) and the managed pointer size in bytes for this target
-  #define FEATURE_EH 0 // To aid platform bring-up, eliminate exceptional EH clauses (catch, filter, filter-handler, fault) and directly execute 'finally' clauses.
+  #define FEATURE_EH 1 // To aid platform bring-up, eliminate exceptional EH clauses (catch, filter, filter-handler, fault) and directly execute 'finally' clauses.
   #define ETW_EBP_FRAMED           1       // if 1 we cannot use REG_FP as a scratch register and must setup the frame pointer for most methods
   #define CSE_CONSTS               1       // Enable if we want to CSE constants
    
